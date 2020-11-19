e5d02f7e9ab885d7d7dc8012946c1e8657b9a06c
JeonJoonHo <jeonjh9@naver.com>
2020-11-19 20:44:38 +0900

    Add skeleton project

---
+++ src/main/java/com/joonho/sprinkle/SprinkleApplication.java
@@ -0,0 +1,13 @@
+package com.joonho.sprinkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinkleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinkleApplication.class, args);
+    }
+
+}
